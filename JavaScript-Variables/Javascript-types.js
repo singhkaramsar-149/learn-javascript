993314a73858b2386872f8f7b39567e4fe3366c8
singhkaramsar-149 <singhkaramsar@gmail.com>
2026-03-25 17:10:16 +0530

    Define Javascript types in javascript-types.js file

---
+++ JavaScript-Variables/Javascript-types.js
@@ -0,0 +1,30 @@
+//In JavaScript, types define the kind of value a variable can hold.
+//  JavaScript has primitive types like String, Number, Boolean, Undefined, Null, Symbol, and BigInt, which store single, immutable values.
+//  For example, "Hello" is a String, 42 is a Number, and true is a Boolean.
+//  Besides primitives, there are non-primitive or reference types, such as Objects, Arrays, Functions, and Dates, which can store multiple values or more complex data.
+//  Objects hold key-value pairs, Arrays store ordered lists, Functions are reusable blocks of code, and Dates represent time values.
+//  Understanding these types is essential for writing correct and efficient JavaScript code.
+
+// String: text
+let Name = "Alice";
+
+// Number: integers or floats
+let age = 25;
+
+// Boolean: true or false
+let isStudent = true;
+
+// Undefined: variable declared but not assigned
+let hobby;
+
+// Null: explicitly no value
+let car = null;
+
+// Symbol: unique identifier
+let id = Symbol("id");
+
+// BigInt: for very large integers
+let bigNumber = 1234567890123456789012345678901234567890n;
+
+//print Values 
+console.log(Name , age , isStudent ,  hobby , car , id , bigNumber);
