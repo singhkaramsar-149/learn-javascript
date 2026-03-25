6e4482d6e9e70281d461d79319b4818863e5ec2f
singhkaramsar-149 <singhkaramsar@gmail.com>
2026-03-25 14:59:06 +0530

    Add Javascript-let.js file inside folder and add some example of let variables

---
+++ JavaScript-Variables/JavaScript-Let.js
@@ -0,0 +1,14 @@
+// let is a keyword in JavaScript used to declare variables that can have their values updated later.
+//  It was introduced in ES6 (ECMAScript 2015) to provide better scoping rules than var.
+//  Variables declared with let are block-scoped, meaning they only exist within the block { } in which they are defined.
+
+//You can change the value of a let variable after declaring it.
+let age = 20;
+age = 15;
+console.log(age);
+//Addition variable
+
+let score = 10;
+score = score + 5;
+console.log(score);
+//Output => 15 
